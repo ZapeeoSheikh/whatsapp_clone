c5d7f14daad8708b3027d383c9f01925cdf04c36
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-27 04:18:03 +0500

    divider update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -130,101 +130,109 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                       return Column(
                         children: [
                           Container(
-                            height: 80,
+                            height: 85,
                             width: MediaQuery.of(context).size.width,
                             color: AppColor.body,
-                            child: Container(
-                              margin: EdgeInsets.only(
-                                right: 10,
-                                left: 10,
-                              ),
-                              alignment: Alignment.center,
-                              child: Row(
-                                children: [
-                                  CircleAvatar(
-                                    backgroundImage:
-                                        AssetImage("img/" + bg[index]),
-                                    radius: 25,
+                            child: Column(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(
+                                    right: 10,
+                                    left: 10,
                                   ),
-                                  Container(
-                                    margin: EdgeInsets.only(left: 15),
-                                    child: Column(
-                                      children: [
-                                        Container(
-                                          margin: EdgeInsets.only(
-                                              top: 20, left: 13),
-                                          child: TabText(
-                                            title[index],
-                                            size: 15,
-                                            color: AppColor.title,
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        Container(
-                                          margin: EdgeInsets.only(left: 5),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            children: [
-                                              Icon(
-                                                Icons.check,
-                                                color: AppColor.icons,
-                                              ),
-                                              Icon(
-                                                Icons.image,
-                                                color: AppColor.icons,
-                                              ),
-                                              TabText(
-                                                "Photo",
-                                                size: 15,
-                                                color: AppColor.unselecttext,
-                                              )
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                  Expanded(child: Container()),
-                                  Column(
+                                  alignment: Alignment.center,
+                                  child: Row(
                                     children: [
+                                      CircleAvatar(
+                                        backgroundImage:
+                                            AssetImage("img/" + bg[index]),
+                                        radius: 25,
+                                      ),
                                       Container(
-                                        margin:
-                                            EdgeInsets.only(top: 20, left: 10),
-                                        child: TabText(
-                                          "Yesterday",
-                                          color: AppColor.unselecttext,
-                                          size: 15,
+                                        margin: EdgeInsets.only(left: 15),
+                                        child: Column(
+                                          children: [
+                                            Container(
+                                              margin: EdgeInsets.only(
+                                                  top: 20, left: 13),
+                                              child: TabText(
+                                                title[index],
+                                                size: 15,
+                                                color: AppColor.title,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 5,
+                                            ),
+                                            Container(
+                                              margin: EdgeInsets.only(left: 5),
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                children: [
+                                                  Icon(
+                                                    Icons.check,
+                                                    color: AppColor.icons,
+                                                  ),
+                                                  Icon(
+                                                    Icons.image,
+                                                    color: AppColor.icons,
+                                                  ),
+                                                  TabText(
+                                                    "Photo",
+                                                    size: 15,
+                                                    color:
+                                                        AppColor.unselecttext,
+                                                  )
+                                                ],
+                                              ),
+                                            )
+                                          ],
                                         ),
                                       ),
-                                      SizedBox(
-                                        height: 3,
-                                      ),
-                                      Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.end,
+                                      Expanded(child: Container()),
+                                      Column(
                                         children: [
-                                          Icon(
-                                            Icons.volume_off_sharp,
-                                            color: AppColor.icons,
+                                          Container(
+                                            margin: EdgeInsets.only(
+                                                top: 20, left: 10),
+                                            child: TabText(
+                                              "Yesterday",
+                                              color: AppColor.unselecttext,
+                                              size: 15,
+                                            ),
                                           ),
                                           SizedBox(
-                                            width: 6,
+                                            height: 3,
                                           ),
-                                          Icon(
-                                            Icons.push_pin_rounded,
-                                            color: AppColor.icons,
-                                          ),
+                                          Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.end,
+                                            children: [
+                                              Icon(
+                                                Icons.volume_off_sharp,
+                                                color: AppColor.icons,
+                                              ),
+                                              SizedBox(
+                                                width: 6,
+                                              ),
+                                              Icon(
+                                                Icons.push_pin_rounded,
+                                                color: AppColor.icons,
+                                              ),
+                                            ],
+                                          )
                                         ],
                                       )
                                     ],
-                                  )
-                                ],
-                              ),
+                                  ),
+                                ),
+                                Divider(
+                                  color: AppColor.unselecttext,
+                                ),
+                              ],
                             ),
-                          )
+                          ),
                         ],
                       );
                     }),
